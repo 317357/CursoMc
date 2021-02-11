bde676e434865d55487409b084fbc7d3a331d66f
Ronaldo <ronaldohenrique374@gmail.com>
2021-02-10 22:03:59 -0300

    Criando operacao de Instanciacao

--- src/main/java/com/ronaldo/CursoMc/domain/Categoria.java
+++ src/main/java/com/ronaldo/CursoMc/domain/Categoria.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+@JsonDeserialize
 @Entity
 public class Categoria implements Serializable {
 	
